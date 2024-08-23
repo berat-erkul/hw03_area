64f5aa8ca847f17dba9f9b279234e58b7abc72d0
berat-erkul <beraterkul00@gmail.com>
2024-08-23 16:29:41 +0300

    m1

---
+++ src/Circle.java
@@ -0,0 +1,21 @@
+public class Circle extends Shape{
+
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                " radius=" + radius +
+                ", area=" + calculateArea() +
+                '}';
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Rectangle r1 = new Rectangle(5,5);
+
+        Circle c1 = new Circle(5);
+
+        System.out.println(c1.toString());
+        System.out.println(r1.toString());
+    }
+}
